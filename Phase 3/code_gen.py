36e891b0a4de38d7636f4a9ff7f24ecb0447cefb
KasraAzizzadeh <119733785+KasraAzizzadeh@users.noreply.github.com>
2025-08-08 23:19:13 +0330

    Update code_gen.py

--- Phase 3/code_gen.py
+++ Phase 3/code_gen.py
@@ -1,60 +1,26 @@
-class ID_entry:
-    def __init__(self, lexeme, type, role, arg_num, scope, address):
-        self.lexeme = lexeme
-        self.type = type
-        self.role = role
-        self.arg_num = arg_num
-        self.scope = scope
-        self.address = address
+from symbol_table import SymbolTable
 
-
-class ScopeStack:
+class SemanticStack:
     def __init__(self):
-        self.scopeStack = []
+        self.stack = []
 
-    def push(self, scope):
-        self.scopeStack.append(scope)
-    
+    def push(self, item):
+        self.stack.append(item)
+
     def pop(self):
-        if self.scopeStack:
-            return self.scopeStack.pop()
+        if not self.is_empty():
+            return self.stack.pop()
         return None
-    
-    def get_scope(self):
-        if self.scopeStack:
-            return self.scopeStack[-1]
-        return None
-        
 
-class SymbolTable:
+    def get_top(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return None
+
+class CodeGen:
     def __init__(self):
-        self.is_declaration = False
-        self.symbols = []
-        self.scopeStack = ScopeStack()
+        self.semantic_stack = SemanticStack()
+        self.symbol_table = SymbolTable()
 
-    def add_symbol(self, lexeme):
-        current_scope = self.scopeStack.get_scope()
-        if current_scope is not None:
-            if self.is_declaration == True:
-                symbol = ID_entry(lexeme, None, None, None, current_scope, None)
-                self.symbols.append(symbol)
-            else:
-                if self.lookup(lexeme) is None:
-                    symbol = ID_entry(lexeme, None, None, None, current_scope, None)
-
-    def lookup(self, lexeme):
-        for symbol in reversed(self.symbols):
-            if symbol.lexeme == lexeme:
-                return symbol
-        return None
-
-    def enter_scope(self):
-        self.scopeStack.push()
-
-    def exit_scope(self):
-        current_scope = self.scopeStack.get_scope()
-        index = len(self.symbols) - 1
-        while index >= 0 and self.symbols[index].scope == current_scope:
-            self.symbols.pop()
-            index -= 1
-        self.scopeStack.pop()
+    def declare():
+        pass
