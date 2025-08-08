a471c5267c60b55330ddd37692bdde8eff6a50b3
KasraAzizzadeh <119733785+KasraAzizzadeh@users.noreply.github.com>
2025-08-08 23:17:56 +0330

    Update code_gen.py

--- Phase 3/code_gen.py
+++ Phase 3/code_gen.py
@@ -1,26 +1,60 @@
-from symbol_table import SymbolTable
+class ID_entry:
+    def __init__(self, lexeme, type, role, arg_num, scope, address):
+        self.lexeme = lexeme
+        self.type = type
+        self.role = role
+        self.arg_num = arg_num
+        self.scope = scope
+        self.address = address
 
-class SemanticStack:
+
+class ScopeStack:
     def __init__(self):
-        self.stack = []
+        self.scopeStack = []
 
-    def push(self, item):
-        self.stack.append(item)
-
+    def push(self, scope):
+        self.scopeStack.append(scope)
+    
     def pop(self):
-        if not self.is_empty():
-            return self.stack.pop()
+        if self.scopeStack:
+            return self.scopeStack.pop()
         return None
-
-    def get_top(self):
-        if not self.is_empty():
-            return self.stack[-1]
+    
+    def get_scope(self):
+        if self.scopeStack:
+            return self.scopeStack[-1]
         return None
+        
 
-class CodeGen:
+class SymbolTable:
     def __init__(self):
-        self.semantic_stack = SemanticStack()
-        self.symbol_table = SymbolTable()
+        self.is_declaration = False
+        self.symbols = []
+        self.scopeStack = ScopeStack()
 
-    def declare():
-        pass
+    def add_symbol(self, lexeme):
+        current_scope = self.scopeStack.get_scope()
+        if current_scope is not None:
+            if self.is_declaration == True:
+                symbol = ID_entry(lexeme, None, None, None, current_scope, None)
+                self.symbols.append(symbol)
+            else:
+                if self.lookup(lexeme) is None:
+                    symbol = ID_entry(lexeme, None, None, None, current_scope, None)
+
+    def lookup(self, lexeme):
+        for symbol in reversed(self.symbols):
+            if symbol.lexeme == lexeme:
+                return symbol
+        return None
+
+    def enter_scope(self):
+        self.scopeStack.push()
+
+    def exit_scope(self):
+        current_scope = self.scopeStack.get_scope()
+        index = len(self.symbols) - 1
+        while index >= 0 and self.symbols[index].scope == current_scope:
+            self.symbols.pop()
+            index -= 1
+        self.scopeStack.pop()
