2b48c584655fc153dd1e0a35c6c9d5b5a4f77efb
KasraAzizzadeh <119733785+KasraAzizzadeh@users.noreply.github.com>
2025-08-10 01:14:35 +0330

    Update code_gen.py

--- Phase 3/code_gen.py
+++ Phase 3/code_gen.py
@@ -24,15 +24,19 @@ class CodeGen:
         self.symbol_table = SymbolTable()
         self.info = CodeGenInfo()
 
+    #semantic actions
     def declare(self):
         self.symbol_table.set_declaration(True)
 
     def push_type(self, type):
-        self.symbol_table.add_empty_symbol_type(type)
+        self.semantic_stack.push(type)
 
     def declare_id(self, lexeme):
-        self.symbol_table.add_symbol_lexeme(lexeme)
-        if self.info.get_counting_func_arg(): # condition to count function arguments
+        type = self.semantic_stack.pop()
+        address = self.get_data_address()
+        self.symbol_table.add_symbol(lexeme, type, address)
+        self.info.last_used_id = lexeme
+        if self.info.arg_declaration == True: # condition to count function arguments
             self.info.inc_func_arg_num()
 
     def pid(self, id):
@@ -48,26 +52,33 @@ class CodeGen:
         arg_num = None
         if has_arg_num:
             arg_num = int(self.semantic_stack.pop()[1:])
+            self.info.increase_data_address(self.info.word_size * (arg_num - 1))
         self.symbol_table.change_to_array(arg_num)
 
-    def declare_func(self):
-        self.symbol_table.change_to_func()
+    def save(self): ## save address of program block for later jump
+        self.semantic_stack.push(self.info.pb_i)
+        self.info.program_block.append("")
+        self.info.pb_i += 1
+    
+    def declare_func(self): ##change type of id to func and set the address of program block cell for function call
+        self.symbol_table.change_to_func(self.info.pb_i)
+        #TODO enter new scorp       
 
     def params_start(self):
         self.info.reset_func_arg_num()
-        self.info.set_counting_func_arg(True)
+        self.info.set_arg_declaration(True)
 
     def params_end(self):
-        self.info.set_counting_func_arg(False)
-        arg_num = self.info.get_func_arg_num()
-        self.info.reset_func_arg_num()
-        self.symbol_table.update_function_arg_num(arg_num)
+        self.info.set_arg_declaration(False)
+        arg_num = self.info.func_arg_num
+        self.symbol_table.update_function_arg_num(arg_num)  #TODO doesnt work because we defined new ids
 
     def func_return(self): ##
         pass
 
-    def save(self): ##
-        pass
+    def func_save_resolve(self):
+        pb_i = self.semantic_stack.pop()
+        self.info.program_block[pb_i] = f"(JP, {self.info.pb_i}, , )"
 
     def jpf_save(self): ##
         pass
@@ -107,3 +118,14 @@ class CodeGen:
 
     def func_call(self): ##
         pass
+
+    #code genrator functions
+    def get_data_address(self):
+        addr = self.info.get_data_address()
+        self.info.increase_data_address(self.info.word_size)
+        return addr
+    
+    def get_temp_address(self):
+        temp = self.info.get_temp_address()
+        self.info.increase_temp_address(self.info.word_size)
+        return temp
