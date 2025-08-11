5e57a74854479631dea68855af9a959cf591e69c
KasraAzizzadeh <119733785+KasraAzizzadeh@users.noreply.github.com>
2025-08-11 19:59:09 +0330

    Update symbol_table.py

--- Phase 3/symbol_table.py
+++ Phase 3/symbol_table.py
@@ -15,84 +15,83 @@ class ID_entry:
 
 
 class ScopeStack:
-    def __init__(self):
-        self.scopeStack = [0]
-
-    def push(self, scope):
-        self.scopeStack.append(scope)
+    def __init__(self, upperStack=None):
+        self.scopeStack = []
+        self.upperStack = upperStack
     
-    def pop(self):
-        if self.scopeStack:
-            return self.scopeStack.pop()
+    def add(self, lexeme, type, address, is_declaration=False):
+        if is_declaration:
+            id_entry = ID_entry(lexeme, type, "var", None, self, address, None)
+            self.scopeStack.append(id_entry)
+            return id_entry
+        id_entry = self.get_IDentry(lexeme)
+        if id_entry:
+            return id_entry
+        else:
+            id_entry = ID_entry(lexeme, type, "var", None, self, address, None)
+            self.scopeStack.append(id_entry)
+            return id_entry
+
+    def get_IDentry(self, lexeme):
+        for entry in self.scopeStack:
+            if entry.lexeme == lexeme:
+                return entry
+        if self.upperStack:
+            return self.upperStack.get_IDentry(lexeme)
         return None
     
-    def get_scope(self):
-        if self.scopeStack:
-            return self.scopeStack[-1]
-        return None
-        
-
-class SymbolTable:
-    def __init__(self):
-        self.is_declaration = False
-        self.symbols = []
-        self.scopeStack = ScopeStack()
-
-    def add_symbol(self, lexeme, type, address):
-        current_scope = self.scopeStack.get_scope()
-        if current_scope is not None:
-            if self.is_declaration == True:
-                symbol = ID_entry(lexeme, type, "var", None, current_scope, address, None)
-                self.symbols.append(symbol)
-                self.is_declaration = False
-            else:
-                if self.lookup(lexeme) is None:
-                    symbol = ID_entry(lexeme, None, "var", None, current_scope, address, None)
-                    self.symbols.append(symbol)
-
-    def change_to_array(self, arg_num):
-        #change last symbol to array
-        symbol = self.symbols[-1] if self.symbols else None
-        if symbol is not None:
-            symbol.role = 'arr'
-            symbol.arg_num = arg_num
-
     def change_to_func(self, jmp_add):
-        #change last symbol to function
-        symbol = self.symbols[-1] if self.symbols else None
+        symbol = self.scopeStack[-1] if self.scopeStack else None
         if symbol is not None:
             symbol.jmp_add = symbol.address
             symbol.address = jmp_add
             symbol.role = 'func'
             symbol.arg_num = 0
 
-    def update_function_arg_num(self, arg_num):
-        symbol = self.symbols[-1] if self.symbols else None
+    def change_to_array(self, arg_num):
+        symbol = self.scopeStack[-1] if self.scopeStack else None
+        if symbol is not None:
+            symbol.role = 'arr'
+            symbol.arg_num = arg_num
+
+    def update_func_arg_num(self, arg_num):
+        symbol = self.scopeStack[-1] if self.scopeStack else None
         if symbol is not None and symbol.role == 'func':
             symbol.arg_num = arg_num
 
+class SymbolTable:
+    def __init__(self):
+        self.is_declaration = False
+        self.symbols = []
+        self.scopes = []
+        self.scopes.append(ScopeStack())
+
+    def add_symbol(self, lexeme, type, address):
+        self.get_current_scope().add(lexeme, type, address, self.is_declaration)
+        self.set_declaration(False)
+
+    def change_to_array(self, arg_num):
+        #change last symbol to array
+        self.get_current_scope().change_to_array(arg_num)
+
+    def change_to_func(self, jmp_add):
+        #change last symbol to function
+        self.get_current_scope().change_to_func(jmp_add)
+
+    def update_function_arg_num(self, arg_num):
+        self.get_current_scope().update_func_arg_num(arg_num)
+
     def lookup(self, lexeme):
-        for symbol in reversed(self.symbols):
-            if symbol.lexeme == lexeme:
-                return symbol
-        return None
+        return self.get_current_scope().get_IDentry(lexeme)
 
     def enter_scope(self):
-        self.scopeStack.push(len(self.symbols))
+        self.scopes.append(ScopeStack(self.scopes[-1]))
 
     def exit_scope(self):
-        current_scope = self.scopeStack.get_scope()
-        index = len(self.symbols) - 1
-        while index >= 0 and self.symbols[index].scope == current_scope:
-            self.symbols.pop()
-            index -= 1
-        self.scopeStack.pop()
+        self.scopes.pop()
+
+    def get_current_scope(self):
+        return self.scopes[-1]
 
     def set_declaration(self, is_declaration):
         self.is_declaration = is_declaration
-
-    def print_symbols(self): # to debug
-        print("Symbol Table:")
-        for symbol in self.symbols:
-            print(symbol.lexeme, symbol.scope)
-        print()
